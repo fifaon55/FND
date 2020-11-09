a48eb96b7ffcb08f981fcf632473bc12c03e848a
fifaon55 <01687600622n@gmail.com>
2020-11-09 13:30:39 +0700

    first commit

---
+++ src/main/webapp/app/modules/home/home.tsx
@@ -0,0 +1,33 @@
+import './home.scss';
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { Row, Col, Alert } from 'reactstrap';
+
+import { IRootState } from 'app/shared/reducers';
+
+export type IHomeProp = StateProps;
+
+export const Home = (props: IHomeProp) => {
+  const { account } = props;
+
+  return (
+    <Row>
+      <Col md="9">
+        <h2>Welcome, {props.account.login}</h2>
+        </Col>
+
+    </Row>
+  );
+};
+
+const mapStateToProps = storeState => ({
+  account: storeState.authentication.account,
+  isAuthenticated: storeState.authentication.isAuthenticated,
+});
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+
+export default connect(mapStateToProps)(Home);
